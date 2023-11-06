58094b88a546b3d520d26f7f0ea4a67016cfdf30
laura <lg8104@gmail.com>
2023-11-06 15:43:52 -0700

    Adding changes to search bar appearance and styles

--- recipes/src/App.jsx
+++ recipes/src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
 import Card from './Card.jsx';
 import Search from './Search.jsx';
-debug-header
-
 
 const App = () => {
   return(
--- recipes/src/Search.jsx
+++ recipes/src/Search.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import "./App.css"
+import "./App.css";
 import logo from "./chefs-hat.svg";
-import { SearchBar } from "./components/SearchBar";
-import { SearchResultsList } from "./components/SearchResultsList";
+import data from "./data.json";
+import { FaSearch } from "react-icons/fa";
 
 function Search() {
   const [results, setResults] = useState([]);
@@ -12,13 +12,66 @@ function Search() {
         <div className="script">
           <div className="search-bar-container">
             <img src={logo} alt="Chef's Hat logo" />
-            <SearchBar setResults={setResults} />
-            {results && results.length > 0 && <SearchResultsList results={results} />}
+            <SearchBar setResults={setResults} data={data} />
+            {results && results.length > 0 && <SearchResultsList 
+            results={results} />}
           </div>
           <h1>Recipes is an app for collecting and organizing your recipes</h1>
         </div>
       </div>
   );
 }
+/* Search Bar */
+const SearchBar = ({ setResults, data }) => {
+  const [input, setInput] = useState("");
+  const fetchData = (value) => {
+      /*fetch ("https://jsonplaceholder.typicode.com/users")
+          .then((response) => response.json())
+          .then((json) => {*/
+      const results = data.filter((recipe) => {
+        return value && recipe && recipe.name &&
+        recipe.name.toLowerCase().includes(value);
+        });
+        setResults(results);
+        console.log("Filtered Results", results);
+      };
+  const handleSearchIconClick = () => {
+    fetchData(input);
+  };
+  const handleChange = (value) => {
+    setInput(value);
+  }
+
+return (
+      <div className="input-wrapper">
+          <input
+              placeholder="Enter an ingredient"
+              value={input}
+              onChange={(e) => handleChange(e.target.value)} />
+          <FaSearch id="search-icon" onClick={handleSearchIconClick} />
+          <label>Search</label>
+      </div>
+  );
+};
+/* Search Bar end */
+
+const SearchResult = ({ result }) => {
+  return (
+      <div className="search-result"
+      onClick={() => alert(`You selected ${result}!`)}>
+      {result}
+      </div>
+  );
+};
+
+const SearchResultsList = ({ results }) => {
+  return (
+      <div className="results-list">
+          {results.map((result, id) => {
+              return <SearchResult result={result.name} key={id} />;
+          })}
+      </div>
+  );
+};
 
 export default Search
