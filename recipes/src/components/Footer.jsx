11448292e7a0578291568d57bb86a4f916b18fba
Tyler Thietje <tylerthietje@gmail.com>
2023-11-07 19:25:16 -0600

    added footer component

---
+++ recipes/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2023 Chingu Group 8. All rights reserved.</p> 
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
